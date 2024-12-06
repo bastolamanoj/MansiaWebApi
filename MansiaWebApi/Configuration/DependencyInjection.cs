9e1e80445c6177f88b64ef54c76ad3c6266f6716
bastolamanoj <manojbastola676@gmail.com>
2024-12-06 23:56:33 +0545

    coonfiguration added for Jwt

---
+++ MansiaWebApi/Configuration/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿namespace MansiaWebApi.Configuration
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            services.AddControllers();
+            return services;
+        }
+
+        //public static IServiceCollection AddAuthentictionAndAuthorization(this IServiceCollection services)
+        //{
+
+        //}
+        
+    }
+}
--- MansiaWebApi/Program.cs
+++ MansiaWebApi/Program.cs
@@ -1,8 +1,69 @@
+using DataProvider.DatabaseContext;
+using DataProvider.Interfaces;
+using DataProvider.Models.Identity;
+using MansiaWebApi.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddPresentation();
+builder.Services.AddEndpointsApiExplorer();
+
+//starting  by registering dbcontext here
+builder.Services.AddDbContext<ApplicationDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection String is not found.."));
+});
+
+//Identity Code
+builder.Services.AddIdentity<User, UserRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddRoles<Role>();
+
+//Jwt Code
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(option =>
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+    }
+
+);
+
+// Add authenticaion to swagger UI  
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddScoped<IAccountRepository, IAccountRepository>();
+
+
+
 
 var app = builder.Build();
 
